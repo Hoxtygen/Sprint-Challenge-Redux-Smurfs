c825d9fca9e722f40bffb62437b8168709f431f2
Hoxtygen <hoxtygen@live.com>
2019-06-21 09:05:26 +0100

    Add reducers

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,19 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import * as types from "../actions/actionTypes";
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+
+ //Your initial/default state for this project could *Although does not have to* look a lot like this
+ const initialState  = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
+   fetching: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
+   error: null,
  }
-*/
+
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +22,25 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export const smurfsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case (types.FETCH): 
+      return {...state, fetching: action.payload};
+      case (types.SUCCESS):
+        return {
+          ...state, 
+          smurfs: action.payload,
+          fetching: false,
+          error: null
+        };
+        case (types.FAILURE):
+          return {
+            ...state, 
+            error: action.payload, 
+            fetching: false
+          }
+    default:
+      return state;
+  }
+};
