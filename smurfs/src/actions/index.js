e4ea876631e8d478894be0a7748b7699d217593a
Hoxtygen <hoxtygen@live.com>
2019-06-21 09:04:53 +0100

    Add action creators

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,7 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import * as types from "./actionTypes";
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +14,41 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export function addSmurfs(smurfs) {
+  return {
+    type: SUCCESS,
+    payload: smurfs,
+  };
+}
+
+export function failure(error) {
+    return {
+        type: FAILURE,
+        payload: error
+    }
+}
+
+export function fetch(works) {
+  return {
+      type: FETCH,
+      payload: works
+  }
+}
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch(fetch(true));
+  //dispatch({type: FETCH})
+  // we code
+  axios.get('http://localhost:3333/smurfs/')
+    .then(res => {
+        console.log(res.data)
+      dispatch(addSwapis(res.data.results)); // option 2
+      //dispatch(fetch(false));
+    })
+    .catch(error => {
+      //dispatch(fetch(false));
+      dispatch(failure(error.message));
+      console.log(error.message);
+    });
+};
